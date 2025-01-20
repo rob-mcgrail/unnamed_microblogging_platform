d9584e5c60fcfaaffd93fc55188261bea0d55516
Rob McGrail <rob.mcgrail@gmail.com>
2025-01-20 02:45:30 +0000

    props

--- app/components/blurt-form.tsx
+++ app/components/blurt-form.tsx
@@ -2,12 +2,18 @@ import {
   useFetcher 
 } from "@remix-run/react";
 
-const BlurtForm = () => {
-  const fetcher = useFetcher();
+export interface BlurtFormProps {
+  id: any;
+  limit: number;
+}
 
+const BlurtForm: React.FC<BlurtFormProps> = ({ id, limit  }) => {
+  const fetcher = useFetcher();
+  console.log(limit);
+  
   return (
     <div className="bg-gray-800 p-4 rounded-lg mb-4">
-      <fetcher.Form action="/blurt" method="post" className="flex flex-col">
+      <fetcher.Form  key={id} action="/blurt" method="post" className="flex flex-col">
         <textarea
           className="bg-gray-700 text-white rounded-lg p-3 mb-3 resize-none focus:outline-none"
           placeholder="What's happening?"
