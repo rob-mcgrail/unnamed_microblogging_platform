3b305159dad95eb64d033d307bfab0f48dc018fe
Rob McGrail <rob.mcgrail@gmail.com>
2025-01-24 08:58:00 +0000

    no more pointless wiring

--- app/components/stats-panel.tsx
+++ app/components/stats-panel.tsx
@@ -3,29 +3,23 @@ import TextCounters from '~/components/text-counters';
 
 export interface StatsPanelProps {
   user: any;
-  textHandlers: any[];
 }
 
-const StatsPanel: React.FC<StatsPanelProps> = ({ user, textHandlers }) => {
+const StatsPanel: React.FC<StatsPanelProps> = ({ user }) => {
 
   return (
     <div className="flex flex-col w-2/5 h-full bg-gray-800 text-white">
       <div className="flex-1 p-4">
         <h1 className="text-xl">{user.name}</h1>
         <p>{user.bio}</p>
-
       </div>
       <div className="flex-1 p-4">
-
-
       </div>
       <div className="flex-1 p-4">
-
-        <TextCounters textHandlers={textHandlers} />
+        <TextCounters />
       </div>
     </div>
   );
-  
 };
 
 export default StatsPanel;
