dcc43e81cdb7ec5a97407ffbc4981b3dcaa66e32
Rob McGrail <rob.mcgrail@gmail.com>
2025-01-26 07:05:17 +0000

    visible submit

--- app/components/post-form.tsx
+++ app/components/post-form.tsx
@@ -17,8 +17,11 @@ const PostForm: React.FC<PostFormProps> = ({ id, limit  }) => {
   const revalidator = useRevalidator();
 
   useEffect(() => {
-    if (fetcher.state === "idle") {
+    if (fetcher.state === "submitting") {
       setContent('');
+      revalidator.revalidate();
+    }
+    if (fetcher.state === "idle") {
       revalidator.revalidate();
     }
   }, [fetcher.state]);
@@ -42,10 +45,39 @@ const PostForm: React.FC<PostFormProps> = ({ id, limit  }) => {
         </p>
         <button
           type="submit"
-          className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 self-end"
+          disabled={fetcher.state === "submitting" || content.length < 1}
+          className={`bg-blue-500 text-white py-2 px-4 rounded-lg self-end ${
+            fetcher.state === "submitting" ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600"
+          }`}
         >
-        POST
-      </button>
+          {fetcher.state === "submitting" ? (
+            <div className="flex items-center justify-center">
+              <svg
+                className="animate-spin h-5 w-5 mr-2 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8H4z"
+                ></path>
+              </svg>
+              ...
+            </div>
+          ) : (
+            "Post"
+          )}
+        </button>
     </fetcher.Form>
   </div>
   );
