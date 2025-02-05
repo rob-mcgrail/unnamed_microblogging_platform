053cbd57753d4702445f1b6f961ce81c735bb5bb
Rob McGrail <rob.mcgrail@gmail.com>
2025-02-05 03:38:50 +0000

    better money code

--- app/routes/money.tsx
+++ app/routes/money.tsx
@@ -2,9 +2,6 @@ import { json, LoaderFunctionArgs } from "@remix-run/node";
 
 import { sessionCookie } from "~/cookies";
 import { redis } from "~/redis.server";
-import { User } from "~/types";
-
-import fetchExistingUser from "~/data/fetch-existing-user.server";
 import fetchUserKeyFromSession from "~/data/fetch-user-key-from-session.server";
 import processEvents from "~/utils/process-events";
 
@@ -12,13 +9,12 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const cookieHeader = request.headers.get("Cookie");
   const sessionId = await sessionCookie.parse(cookieHeader);
   const userKey = await fetchUserKeyFromSession(sessionId);
-  const data = await fetchExistingUser(userKey) as { user: User };
   const events = await redis.lrange(`events:${userKey}`, 0, -1);
   await redis.del(`events:${userKey}`);
-  let money = data.user.money;
-  
+  let money = await redis.hget(`user:${userKey}`, 'money');
+
   if (events.length > 0) {
-    money = processEvents(data.user as User, events as string[], []);
+    money = processEvents(money, userKey, events as string[], []);
     await redis.hset(`user:${userKey}`, { money });
   }
 
--- app/utils/process-events.tsx
+++ app/utils/process-events.tsx
@@ -1,15 +1,16 @@
 import { User } from "../types";
 
 const process = (
-  user: User,
+  startMoney: number,
+  userKey: string,
   events: string[],
   modifiers: any[]
 ): number => {
-  let money = parseInt(user.money as unknown as string);
+  let money = parseInt(startMoney as unknown as string);
 
   events.forEach((e) => {
     const event = JSON.parse(e);
-    if (event.subject == user.id) {
+    if (event.subject == userKey) {
 
       modifiers.forEach((mod) => {
         // Something in here but for now...
